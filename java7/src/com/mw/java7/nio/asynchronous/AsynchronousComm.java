5f15108997f14a81c4ae63e44473dffe6a6693a4
Allen <huxiaoming@live.com>
2017-04-07 16:46:59 +0800

    rename the package

---
+++ java7/src/com/mw/java7/nio/asynchronous/AsynchronousComm.java
@@ -0,0 +1,49 @@
+package com.mw.java7.nio.asynchronous;
+
+import java.nio.ByteBuffer;
+import java.nio.channels.AsynchronousServerSocketChannel;
+import java.nio.channels.AsynchronousSocketChannel;
+import java.nio.channels.CompletionHandler;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+
+public class AsynchronousComm {
+	
+	// List<AsynchronousSocketChannel> _sockets = Collections.synchronizedList(new LinkedList<AsynchronousSocketChannel>());
+	
+	List<IMyProtocol> _protocols = Collections.synchronizedList(new LinkedList<IMyProtocol>());
+	
+	public void registerSocket(String type, AsynchronousSocketChannel socket) {
+		IMyProtocol protocol = IMyProtocol.getNewServerSideInstance(type, socket);
+		_protocols.add(protocol);
+		protocol.start();
+	}
+	
+	public void removeSocket(AsynchronousSocketChannel socket) {
+		//_sockets.remove(socket);
+	}
+	
+		
+	public class CommThread implements Runnable {
+		
+		
+		public CommThread() {
+			
+		}
+		
+		@Override
+		public void run() {
+			while (true) {
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+}
---
+++ java7/src/com/mw/java7/regex/TestHarness.java
@@ -0,0 +1,58 @@
+package com.mw.java7.regex;
+
+import java.io.BufferedReader;
+import java.io.Console;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Formatter;
+import java.util.Locale;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
+public class TestHarness {
+	public static void main(String[] args) {
+
+		while (true) {
+
+			StringBuilder sb = new StringBuilder();
+			// Send all output to the Appendable object sb
+			Formatter formatter = new Formatter(sb, Locale.US);
+
+			Pattern pattern;
+			try {
+				pattern = Pattern.compile(readLine("%nEnter your regex: "));
+				Matcher matcher = pattern
+						.matcher(readLine("Enter input string to search: "));
+
+				boolean found = false;
+				while (matcher.find()) {
+
+					formatter.format("I found the text"
+							+ " \"%s\" starting at "
+							+ "index %d and ending at index %d.%n",
+							matcher.group(), matcher.start(), matcher.end());
+					found = true;
+				}
+				if (!found) {
+					formatter.format("No match found.%n");
+				}
+				System.out.print(sb);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+		}
+	}
+
+	private static String readLine(String format, Object... args)
+			throws IOException {
+		if (System.console() != null) {
+			return System.console().readLine(format, args);
+		}
+		System.out.print(String.format(format, args));
+		BufferedReader reader = new BufferedReader(new InputStreamReader(
+				System.in));
+		return reader.readLine();
+	}
+}
