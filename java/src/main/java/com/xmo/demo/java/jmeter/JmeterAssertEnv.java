f9bd5e5e7289ea72af86a61515361cd7b2b86605
Hu,Xiaoming <huxiaoming@live.com>
2017-08-11 15:59:25 +0800

    update for jmeter function

--- java/src/main/java/com/xmo/demo/java/jmeter/JmeterAssertEnv.java
+++ java/src/main/java/com/xmo/demo/java/jmeter/JmeterAssertEnv.java
@@ -2,6 +2,7 @@ package com.xmo.demo.java.jmeter;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -24,10 +25,39 @@ public class JmeterAssertEnv {
     }
 
     public static void main(String[] args) {
-        parseJsonUser();
+        // parseJsonUser();
+        JmeterAssertEnv env = new JmeterAssertEnv();
+
+        String responseJson = "{\"errorCode\": \"SUCCEEDED\", \"nickname\": \"nickname\"}";
+        String user = "{\"mobilePhone\":{\"number\":\"15216662328\"},"
+                + "\"wechatId\":\"\",\"wechatName\":\"\",\"nickname\":\"nickname\","
+                + "\"passwd\":\"password\",\"verificationCode\":{\"code\":\"verificationCode\"}}";
+        JSONObject jb2 = JSONObject.fromObject(user);
+        env.checkUserGetResponse(responseJson, "SUCCEEDED", jb2);
     }
 
-    public static void parseJsonUser() {
+    boolean checkLoginResponse(String response, String expectedErroCode) {
+        JSONObject jb = JSONObject.fromObject(response);
+
+        String errorCode = jb.getString("errorCode");
+        if (!expectedErroCode.equals(errorCode)) {
+            Failure = true;
+            FailureMessage = "The expectedErroCode is: " + expectedErroCode +
+                    ", but we got: " + errorCode;
+        } else if (expectedErroCode.equals("SUCCEEDED")) {
+            JSONObject accessTokenObj = (JSONObject) jb.get("accessToken");
+            String accessToken = accessTokenObj.getString("token");
+            if (accessToken == null || accessToken.isEmpty()) {
+                Failure = true;
+                FailureMessage = "The accessToken should not empty";
+            } else {
+                vars.put("current_access_token", accessToken);
+            }
+        }
+        return !Failure;
+    }
+
+    public void parseJsonUser() {
         String responseJson = "{\"errorCode\": \"SUCCEEDED\", \"userId\": \"12\"}";
         JSONObject jb = JSONObject.fromObject(responseJson);
 
@@ -68,21 +98,24 @@ public class JmeterAssertEnv {
             Failure = true;
             FailureMessage = "The expectedErroCode is: " + expectedErroCode +
                     ", but we got: " + errorCode;
-        }
-        String userId = jb.getString("userId");
-        vars.put("current_user_id", userId);
-        if (expectedUserId.equals("positive integer")) {
-            long userIdParased = Long.parseLong(userId);
-            if (userIdParased < 0) {
-                Failure = true;
-                FailureMessage = "The userId is expected as: " + expectedUserId
-                        + ", but it is: " + userId;
-            }
-        } else {
-            if (!expectedUserId.equals(userId)) {
-                Failure = true;
-                FailureMessage = "The userId is expected as: " + expectedUserId
-                        + ", but it is: " + userId;
+        } else if (expectedErroCode.equals("SUCCEEDED")) {
+            String userId = jb.getString("userId");
+            vars.put("current_user_id", userId);
+            if (expectedUserId.equals("positive integer")) {
+                long userIdParased = Long.parseLong(userId);
+                if (userIdParased < 0) {
+                    Failure = true;
+                    FailureMessage = "The userId is expected as: " + expectedUserId
+                            + ", but it is: " + userId;
+                    log.error(FailureMessage);
+                }
+            } else {
+                if (!expectedUserId.equals(userId)) {
+                    Failure = true;
+                    FailureMessage = "The userId is expected as: " + expectedUserId
+                            + ", but it is: " + userId;
+                    log.error(FailureMessage);
+                }
             }
         }
         return !Failure;
@@ -107,9 +140,25 @@ public class JmeterAssertEnv {
     }
 
     boolean isJsonFieldEqual(JSONObject j1, JSONObject j2, String field) {
-        String j1v = j1.getString(field);
-        String j2v = j1.getString(field);
+        String j1v = null;
+        String j2v = null;
+        try {
+            j1v = j1.getString(field);
+        } catch (Exception t) {
+
+        }
+
+        try {
+            j2v = j2.getString(field);
+        } catch (Exception t) {
+
+        }
+
         if (j1v == j2v && j1v == null) {
+            return true;
+        } else if (j1v != null && j2v == null && j1v.isEmpty()) {
+            return true;
+        } else if (j1v == null && j2v != null && j2v.isEmpty()) {
             return true;
         } else if (j1v != null && j2v != null) {
             return j1v.equals(j2v);
@@ -127,18 +176,40 @@ public class JmeterAssertEnv {
             FailureMessage = "The expectedErroCode is: " + expectedErroCode +
                     ", but we got: " + errorCode;
         } else {
-            jb.keySet();
-            if (isJsonFieldEqual(jb, expectedUser, "photo")
-                    && isJsonFieldEqual(jb, expectedUser, "nickname")
-                    && isJsonFieldEqual(jb, expectedUser, "wechatId")
-                    && isJsonFieldEqual(jb, expectedUser, "wechatName")) {
-
-            } else {
+            jb.remove("errorCode"); // we dont want to compre the errorCode
+            String[] mandatoryKeys = { "photo", "nickname", "wechatId", "wechatName" };
+            if (!isJsonSubset(jb, expectedUser, mandatoryKeys)) {
                 Failure = true;
-                FailureMessage = "The expectedErroCode is: " + expectedErroCode +
-                        ", but we got: " + errorCode;
+                FailureMessage = "The expectedUser is: " + expectedUser +
+                        ", but we got: " + response;
             }
         }
         return !Failure;
+    }
+
+    // Is jsonObj1 is an subset of jsonObj2?
+    boolean isJsonSubset(JSONObject jsonObj1, JSONObject jsonObj2,
+            String[] mandatoryKeys) {
+        Map keyCompared = new HashMap();
+        Set keySet = jsonObj1.keySet();
+        for (Object key : keySet) {
+            String keyStr = (String) key;
+            keyCompared.put(keyStr, true);
+            if (isJsonFieldEqual(jsonObj1, jsonObj2, keyStr)) {
+                continue;
+            } else {
+                return false;
+            }
+        }
+
+        for (String mandatoryKey : mandatoryKeys) {
+            if (!keyCompared.containsKey(mandatoryKey)) {
+                if (!isJsonFieldEqual(jsonObj1, jsonObj2, mandatoryKey)) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
     }
 }
