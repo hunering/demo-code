ff0693a2bd380d18ea3d0ffe886f4c45cc5a52fc
Hu, Xiaoming <huxiaoming@live.com>
2018-10-15 14:29:56 +0800

    for stdlib part2

---
+++ python/11-stdlib2.py
@@ -0,0 +1,123 @@
+# output format
+import reprlib
+print(reprlib.repr(set('supercalifragilisticexpialidocious')))
+
+import pprint
+t = [[[['black', 'cyan'], 'white', ['green', 'red']], [['magenta',
+    'yellow'], 'blue']]]
+
+pprint.pprint(t, width=30)
+
+import textwrap
+doc = """The wrap() method is just like fill() except that it returns
+a list of strings instead of one big string with newlines to separate
+the wrapped lines."""
+
+print(textwrap.fill(doc, width=40))
+
+
+# template
+from string import Template
+t = Template('${village}folk send $$10 to $cause.')
+print(t.substitute(village='Nottingham', cause='the ditch fund'))
+
+t = Template('Return the $item to $owner.')
+d = dict(item='unladen swallow')
+print(t.safe_substitute(d))
+
+class BatchRename(Template):
+    delimiter = '%'
+
+t  = BatchRename("%{village}folk send %%10 to %cause.")
+print(t.substitute(village='Nottingham', cause='the ditch fund'))
+
+# Working with Binary Data Record Layouts
+# Pack codes "H" and "I" represent two and four byte 
+# unsigned numbers respectively. 
+# The "<" indicates that they are standard size 
+# and in little-endian byte order
+import struct
+
+with open('myfile.zip', 'rb') as f:
+    data = f.read()
+
+start = 0
+for i in range(3):                      # show the first 3 file headers
+    start += 14
+    fields = struct.unpack('<IIIHH', data[start:start+16])
+    crc32, comp_size, uncomp_size, filenamesize, extra_size = fields
+
+    start += 16
+    filename = data[start:start+filenamesize]
+    start += filenamesize
+    extra = data[start:start+extra_size]
+    print(filename, hex(crc32), comp_size, uncomp_size)
+
+    start += extra_size + comp_size     # skip to the next header
+
+# threading
+import threading, zipfile
+
+class AsyncZip(threading.Thread):
+    def __init__(self, infile, outfile):
+        threading.Thread.__init__(self)
+        self.infile = infile
+        self.outfile = outfile
+
+    def run(self):
+        f = zipfile.ZipFile(self.outfile, 'w', zipfile.ZIP_DEFLATED)
+        f.write(self.infile)
+        f.close()
+        print('Finished background zip of:', self.infile)
+
+background = AsyncZip('mydata.txt', 'myarchive.zip')
+background.start()
+print('The main program continues to run in foreground.')
+
+background.join()    # Wait for the background task to finish
+print('Main program waited until background was done.')
+
+# logging
+import logging
+logging.debug('Debugging information')
+logging.info('Informational message')
+logging.warning('Warning:config file %s not found', 'server.conf')
+logging.error('Error occurred')
+logging.critical('Critical error -- shutting down')
+
+# weak reference
+import weakref, gc
+class A:
+    def __init__(self, value):
+        self.value = value
+    def __repr__(self):
+        return str(self.value)
+
+a = A(10)                   # create a reference
+d = weakref.WeakValueDictionary()
+d['primary'] = a            # does not create a reference
+print(d['primary'])               # fetch the object if it is still alive
+del a                       # remove the one reference
+gc.collect()                # run garbage collection right away
+
+# print(d['primary'])         # entry was automatically removed, will throw exception
+
+# The array module provides an array() object 
+# that is like a list that stores only homogeneous data 
+# and stores it more compactly.
+from array import array
+a = array('H', [4000, 10, 700, 22222])
+print(sum(a))
+print(a[1:3])
+
+from collections import deque
+d = deque(["task1", "task2", "task3"])
+d.append("task4")
+print("Handling", d.popleft())
+
+# heap
+from heapq import heapify, heappop, heappush
+data = [1, 3, 5, 7, 9, 2, 4, 6, 8, 0]
+heapify(data)                      # rearrange the list into heap order
+heappush(data, -5)                 # add a new entry
+print([heappop(data) for i in range(3)])  # fetch the three smallest entries
