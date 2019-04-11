0597a39db5b9c538856cb1ae45e4a13b96a570ba
Hu,Xiaoming <huxiaoming@live.com>
2019-04-11 16:17:36 +0800

    more code

--- python/books/DLWP/7.1.1-function-api.py
+++ python/books/DLWP/7.1.1-function-api.py
@@ -1,5 +1,20 @@
+# %%
 from keras import Input, layers
+from keras import Model
 
-input_tensor = Input(shape=(32,))
-dense = layers.Dense(32, activation='relu')
-output_tensor = dense(Input)
+input_tensor = Input(shape=(64,))
+x = layers.Dense(32, activation='relu')(input_tensor)
+x = layers.Dense(32, activation='relu')(x)
+output_tensor = layers.Dense(10, activation='softmax')(x)
+model = Model(input_tensor, output_tensor)
+model.summary()
+
+# %%
+import numpy as np
+model.compile(optimizer='rmsprop', loss='categorical_crossentropy')
+
+x_train = np.random.random((1000, 64))
+y_train = np.random.random((1000, 10))
+
+model.fit(x_train, y_train, epochs=10, batch_size=128)
+score = model.evaluate(x_train, y_train)
